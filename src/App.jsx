8501446dd98ed91656bed0e3c89837d08d304d48
pashamakhilkumarreddy <akhil.reddy@frequence.com>
2020-04-15 23:55:15 +0530

    Add Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Router from './routes';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <main className="container">
+        <Router />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
